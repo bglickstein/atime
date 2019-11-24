a3ce959408b40e109b9bd1179b63c318e089d7a5
Bob Glickstein <bobg@emphatic.com>
2019-11-23 17:17:55 -0800

    Implement io.ReaderAt.

--- file.go
+++ file.go
@@ -8,14 +8,21 @@ import (
 	orig "github.com/djherbis/atime"
 )
 
-// File satisfies io.Reader, Closer, and Seeker. Stats the
-// file before opening it and restores the mtime and atime when
-// closing it.
+// File satisfies io.Reader, ReaderAt, Closer, and Seeker.
+// Stats the file before opening it
+// and restores the mtime and atime when closing it.
 type File struct {
+	io.ReadCloser
+	io.ReaderAt
+	io.Seeker
+
 	f            *os.File
 	mtime, atime time.Time
 }
 
+// Open opens a new File for reading.
+// The caller should close the File when finished.
+// Closing will restore the file's mtime and atime.
 func Open(path string) (*File, error) {
 	fi, err := os.Stat(path)
 	if err != nil {
@@ -32,14 +39,23 @@ func Open(path string) (*File, error) {
 	}, nil
 }
 
+// Read implements io.Reader.
 func (a File) Read(p []byte) (int, error) {
 	return a.f.Read(p)
 }
 
+// ReadAt implements io.ReaderAt.
+func (a File) ReadAt(p []byte, off int64) (int, error) {
+	return a.f.ReadAt(p, off)
+}
+
+// Seek implements io.Seeker.
 func (a File) Seek(offset int64, whence int) (int64, error) {
 	return a.f.Seek(offset, whence)
 }
 
+// Close closes the file,
+// restoring the mtime and atime to what they were when the file was first opened.
 func (a File) Close() error {
 	path := a.f.Name()
 	err := a.f.Close()
